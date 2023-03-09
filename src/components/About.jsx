f677f4a1a2abe2cae5c2eaf920470c957d5545f4
CalumMcCallion <mccallioncalum@gmail.com>
2023-03-09 20:32:21 +0000

    progress on component development

---
+++ src/components/About.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <Tilt className="sm:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+            <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+      I'm a Computer Science student and I've been working with a bunch of different programming languages and technologies. 
+      Some of which include Python, ReactJS, CSS, Javascript, C++, and Java. 
+      I've spent a lot of time learning and practicing with these tools, 
+      I've really enjoyed exploring the different languages and technologies that are out there, and I'm always on 
+      the lookout for new things to learn. It's been really exciting to see how each of these tools can be used to create all kinds of different software and web applications.
+      As a computer science student, I know that there's always more to learn and new skills to develop. 
+      But I'm excited about the progress I've made so far and I'm looking forward to seeing where my studies will take me in the future.
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service}/>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about");
